716a30a5783c2dffba2e9aca45fc5b9313ec1566
Oduzz <tobiloba428@gmail.com>
2025-04-10 04:58:36 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Greetings from "./component/Greetings.jsx"
+
+function App() {
+  return (
+    <>
+      <Greetings />
+    </>
+  )
+}  
+
+export default App;
---
+++ src/component/Greetings.jsx
@@ -0,0 +1,12 @@
+function Greetings(){ {
+
+    // Hello world component
+    return (
+        <div className="greet">
+            <h1>{"Hello World!"}</h1>
+        </div>
+    )
+}
+
+}
+export default Greetings;
